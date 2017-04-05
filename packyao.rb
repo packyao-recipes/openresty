f0766eb449f60ab126c828b2b6b08e1bf162e3bc
Ameir Abdeldayem <ameirh@gmail.com>
2017-04-05 10:42:57 -0400

    Update Openresty version.

--- packyao.rb
+++ packyao.rb
@@ -1,6 +1,6 @@
 require 'json'
 
-version = '1.9.3.2'
+version = '1.11.2.2'
 
 a = {}
 a['name'] = 'openresty'
@@ -10,7 +10,7 @@ a['commands'] = [
   <<-EOF
   apt-get update
   apt-get -y install wget libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make build-essential
-  wget -O source https://openresty.org/download/ngx_openresty-#{version}.tar.gz
+  wget -O source https://openresty.org/download/openresty-#{version}.tar.gz
   tar xfz source
   cd ngx_openresty-#{version}
   ./configure && make && make install
@@ -18,7 +18,7 @@ a['commands'] = [
   chmod 755 /etc/init.d/openresty
   EOF
 ]
-a['description'] = 'ngx_openresty is a full-fledged web application server by bundling the standard nginx core, lots of 3rd-party nginx modules, as well as most of their external dependencies.'
+a['description'] = 'openresty is a full-fledged web application server by bundling the standard nginx core, lots of 3rd-party nginx modules, as well as most of their external dependencies.'
 a['depends'] = []
 a['cwd'] = ''
 a['env'] = {}
@@ -30,4 +30,4 @@ a['package_files'] = {
 a['build_distro'] = 'ubuntu'
 a['build_distro_version'] = '14.04'
 
-File.write('packyao.json', JSON.pretty_generate(a))
+File.write("#{File.dirname(__FILE__)}/packyao.json", JSON.pretty_generate(a))
